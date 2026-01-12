dd757b79b62e2e92620b455e523cc0a20068e925
kevanadlard <59034329+kadlard@users.noreply.github.com>
2026-01-12 13:00:30 -0500

    bit more cleanup

--- frontend/src/stories/Tests/utils/PageRenderer.jsx
+++ frontend/src/stories/Tests/utils/PageRenderer.jsx
@@ -1,15 +1,8 @@
 import React from "react";
 import { PAGES } from "../../../utils/constants.jsx";
 
-import DeleteMFAPage from "../../../features/MFAPhoneNumber/DeleteMFAPhoneNumber/component/DeleteMFAPage.jsx";
-import DeleteMFAPhoneNumberConfirm from "../../../features/MFAPhoneNumber/DeleteMFAPhoneNumber/component/DeleteMFAPhoneNumberConfirm.jsx";
-
 const PageRenderer = ({ page, ...props }) => {
   switch (page) {
-    case PAGES.deleteMFAPage:
-      return <DeleteMFAPage />;
-    case PAGES.deleteMFAPhoneNumberConfirm:
-      return <DeleteMFAPhoneNumberConfirm {...props} />;
     default:
       return null;
   }
