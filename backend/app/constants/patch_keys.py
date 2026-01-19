e2f72e29b26a4c6501099273bda6daed5e2e3183
kevanadlard <59034329+kadlard@users.noreply.github.com>
2026-01-19 13:23:44 -0500

    Start PRBranch empty

--- backend/app/constants/patch_keys.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class PatchKeys(str, Enum):
-    LEGACY_PAI_DATA_KEY = "legacypaidata"
-    AUDIT_DATA_KEY = "auditdata"
-    PROCESSING_DATA_KEY = "processingdata"
--- backend/app/utils/schemas.py
+++
@@ -1,8 +0,0 @@
-from typing import Any, Optional
-from pydantic import BaseModel
-
-
-class ResponseModel(BaseModel):
-    success: bool
-    message: str
-    data: Optional[Any] = None
