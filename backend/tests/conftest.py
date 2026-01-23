782fd294d763c39d7046ff378d3ea121cbf080bc
cds-yiwei <yiwei.wang@cds-snc.ca>
2026-01-23 10:37:23 -0800

    fix (backend): move migration_rp.json to .env (#71)
    * move migration_rp.json to .env
    
    * make pytest working

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -7,3 +7,12 @@ os.environ.setdefault("IBM_VERIFY_MIGRATION_API_CLIENT_ID", "test-client-id")
 os.environ.setdefault("IBM_VERIFY_MIGRATION_API_SECRET", "test-client-secret")
 os.environ.setdefault("IBM_VERIFY_MIGRATION_CLIENT_ID", "test-profile-client-id")
 os.environ.setdefault("IBM_VERIFY_MIGRATION_SECRET", "test-profile-client-secret")
+
+# Provide required RP (Relying Party) configuration for imports during test collection.
+os.environ.setdefault("RP_CLIENT_ID", "test-rp-client-id")
+os.environ.setdefault("RP_CLIENT_NAME", "Test RP Client")
+os.environ.setdefault("RP_CLIENT_NAME_EN", "Test RP Client EN")
+os.environ.setdefault("RP_CLIENT_NAME_FR", "Test RP Client FR")
+os.environ.setdefault("RP_REDIRECT_URI", "http://localhost:3000/callback")
+os.environ.setdefault("RP_REDIRECT_URI_EN", "http://localhost:3000/en/callback")
+os.environ.setdefault("RP_REDIRECT_URI_FR", "http://localhost:3000/fr/callback")
