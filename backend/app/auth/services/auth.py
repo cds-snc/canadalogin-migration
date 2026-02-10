dbba771c6ea36db8425a4437b111cebfca503e6f
kevanadlard <kevan.adlard@cds-snc.ca>
2026-02-10 16:32:18 -0500

    fix: refixed variable name to MIGRATION_SOLUTION_DOMAIN (#97)
    * refixed variable name to MIGRATION_SOLUTION_DOMAIN
    
    * removed unused import
    
    * fixed lint errors
    
    ---------
    
    Co-authored-by: kevanadlard <59034329+kadlard@users.noreply.github.com>

--- backend/app/auth/services/auth.py
+++ backend/app/auth/services/auth.py
@@ -40,10 +40,10 @@ async def get_http_client(request: Request) -> AsyncClient:
 
 def get_base_profile_management_url():
     config = get_configuration()
-    redirectValue = config.MIGRATON_SOLUTION_DOMAIN
+    redirectValue = config.MIGRATION_SOLUTION_DOMAIN
 
     if config.ENVIRONMENT != "local":
-        redirectValue = f"https://{config.MIGRATON_SOLUTION_DOMAIN}"
+        redirectValue = f"https://{config.MIGRATION_SOLUTION_DOMAIN}"
     return redirectValue
 
 
@@ -124,7 +124,7 @@ async def callback_handler(request: Request, lang: str):
             redirectValue = f"{redirectValue}/{lang}"
 
         logger.info("OIDC Callback Handler")
-        logger.info(f"Redirect to MIGRATON_SOLUTION_DOMAIN: {redirectValue}")
+        logger.info(f"Redirect to MIGRATION_SOLUTION_DOMAIN: {redirectValue}")
         return RedirectResponse(url=redirectValue)
     except OAuthError as error:
         logger.error(f"OAuth error: {error}")
