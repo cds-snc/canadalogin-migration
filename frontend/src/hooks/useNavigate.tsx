453ce61cecc53d4be61f34af0a32507e2c49d7e2
kevanadlard <kevan.adlard@cds-snc.ca>
2026-02-26 10:21:02 -0500

    fix(language): Feature/possible language toggle fix (#138)
    * fix(lang): possible race condition for laguage toggle issue
    
    * fixed formating
    
    ---------
    
    Co-authored-by: kevanadlard <59034329+kadlard@users.noreply.github.com>

--- frontend/src/hooks/useNavigate.tsx
+++ frontend/src/hooks/useNavigate.tsx
@@ -1,7 +1,11 @@
+import { useCallback } from "react";
 import { useNavigate } from "react-router";
 
 export function useNavigateHelper() {
   const navigate = useNavigate();
-  return (path: string, replaceHistory: boolean = false, state?: any) =>
-    navigate(path, { replace: replaceHistory, state });
+  return useCallback(
+    (path: string, replaceHistory: boolean = false, state?: any) =>
+      navigate(path, { replace: replaceHistory, state }),
+    [navigate],
+  );
 }
